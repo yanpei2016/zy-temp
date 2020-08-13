c1c3994fd022ad3fac66227ddc843a466070325d
严佩 <pei.yan@haoji-hr.com>
2020-08-13 15:29:35 +0800

    模板格式化

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        <% if (useVueRouter) { %>
-          <router-view></router-view>
-        <% } %>
+        <%_ if (useVueRouter) { %><router-view></router-view><% } _%>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,35 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-<% if (useVueRouter) { %>
-  import router from './router'
-<% } %>
-
-<% if (useVuex) { %>
-  import store from './store'
-<% } %>
-
-<% if(UIfrag==='eleUI'){%>
-  import ElementUI from 'element-ui'
-  Vue.use(ElementUI)
-<% } %>
-<% if(UIfrag==='vant'){%>
-  import vant from 'vant'
-  Vue.use(vant)
-<% } %>
-<% if(UIfrag==='antVue'){%>
-  import Antd  from 'ant-design-vue'
-  import 'ant-design-vue/dist/antd.css';
-  Vue.use(Antd)
-<% } %>
+<%_ if (useVueRouter) { _%>import router from './router'<%_ } _%>
+<%_ if (useVuex) { _%>import store from './store'<%_ } _%>
+<%_ if(UIfrag==='eleUI'){ _%>import ElementUI from 'element-ui' 
+Vue.use(ElementUI)<%_ } _%>
+<%_ if(UIfrag==='vant'){ _%>import vant from 'vant'
+Vue.use(vant)<%_ } _%>
+<%_ if(UIfrag==='antVue'){ _%>import Antd  from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd)
+<%_ } _%>
 
 
 Vue.config.productionTip = false
 new Vue({
-  <% if(useVueRouter){%>
-    router,
-  <% } %>
-  <% if(useVuex){%>
-    store,
-  <% } %>
+  <%_ if(useVueRouter){ _%>
+    router, <%_ } _%>
+  <%_ if(useVuex){ _%>
+    store,<%_ } _%>
   render: function (h) { return h(App) },
 }).$mount('#app')
