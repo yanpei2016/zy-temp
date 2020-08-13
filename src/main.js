349dd19225825744f6cb054e4e570f04f99760a5
严佩 <pei.yan@haoji-hr.com>
2020-08-13 16:44:04 +0800

    格式化

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
-<%_ if (useVueRouter) { _%>import router from './router' <% } %>
-<%_ if (useVuex) { _%>import store from './store' <% } %>
-<%_ if(UIfrag==='eleUI'){ _%>import ElementUI from 'element-ui' <% } %>
-<%_ if(UIfrag==='eleUI'){ _%>Vue.use(ElementUI)<% } %>
-<%_ if(UIfrag==='vant'){ _%>import vant from 'vant'<% } %>
-<%_ if(UIfrag==='vant'){ _%>Vue.use(vant)<% } %>
-<%_ if(UIfrag==='antVue'){ _%>import Antd  from 'ant-design-vue';<% } %>
-<%_ if(UIfrag==='antVue'){ _%>import 'ant-design-vue/dist/antd.css';<% } %>
-<%_ if(UIfrag==='antVue'){ _%>Vue.use(Antd)<% } %>
+<%_ if (useVueRouter) { _%>import router from './router' 
+<%_ } _%>
+<%_ if (useVuex) { _%>import store from './store' 
+<%_ } _%>
+<%_ if(UIfrag==='eleUI'){ _%>import ElementUI from 'element-ui' 
+<%_ } _%>
+<%_ if(UIfrag==='eleUI'){ _%>Vue.use(ElementUI)
+<%_ } _%>
+<%_ if(UIfrag==='vant'){ _%>import vant from 'vant'
+<%_ } _%>
+<%_ if(UIfrag==='vant'){ _%>Vue.use(vant)
+<%_ } _%>
+<%_ if(UIfrag==='antVue'){ _%>import Antd  from 'ant-design-vue';
+<%_ } _%>
+<%_ if(UIfrag==='antVue'){ _%>import 'ant-design-vue/dist/antd.css';
+<%_ } _%>
+<%_ if(UIfrag==='antVue'){ _%>Vue.use(Antd)
+<%_ } _%>
 
 
 Vue.config.productionTip = false
 new Vue({
   <%_ if(useVueRouter){ _%>router,
-  <% } %>
+  <%_ } _%>
   <%_ if(useVuex){_%>store,
   <%_ } _%>
   render: function (h) { return h(App) },
