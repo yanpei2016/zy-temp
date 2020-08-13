9fb1fc96d4d1670f7f2a3b7f4c2ea3f450b72c4d
严佩 <pei.yan@haoji-hr.com>
2020-08-13 14:39:10 +0800

    删除lock信息

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+<% if (useVueRouter) { %>
+  import router from './router'
+<% } %>
+
+<% if (useVuex) { %>
+  import store from './store'
+<% } %>
+
+<% if(UIfrag==='eleUI'){%>
+  import ElementUI from 'element-ui'
+  Vue.use(ElementUI)
+<% } %>
+<% if(UIfrag==='vant'){%>
+  import vant from 'vant'
+  Vue.use(vant)
+<% } %>
+<% if(UIfrag==='antVue'){%>
+  import Antd  from 'ant-design-vue'
+  import 'ant-design-vue/dist/antd.css';
+  Vue.use(Antd)
+<% } %>
+
 
 Vue.config.productionTip = false
-
 new Vue({
+  <% if(useVueRouter){%>
+    router,
+  <% } %>
+  <% if(useVuex){%>
+    store,
+  <% } %>
   render: function (h) { return h(App) },
 }).$mount('#app')
