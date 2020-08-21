1e0f6b8d4da57ee2e88eedd0aef553a32c66e712
严佩 <pei.yan@haoji-hr.com>
2020-08-21 17:30:28 +0800

    .

--- babel.config.js
+++ babel.config.js
@@ -2,39 +2,39 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-    <%_ if(UIfrag==='vant'){ _%>
-    plugins: [
-        [
-            "import", 
-            {
-                "libraryName": "vant",
-                "libraryDirectory": "es",
-                "style": true
-            }
-        ]
-    ]
-    <%_ } _%>
-    <%_ if(UIfrag==='antVue'){ _%>
-    plugins: [
-        [
-            "import",
-             { 
-                "libraryName": "ant-design-vue", 
-                "libraryDirectory": "es", 
-                "style": "css" 
-            }
-        ] // `style: true` 会加载 less 文件
-    ]
-    <%_ } _%>
-    <%_ if(UIfrag==='eleUI'){ _%>
-    "plugins": [
+  <%_ if(UIfrag==='vant'){ _%>
+  plugins: [
       [
-        "component",
-        {
-          "libraryName": "element-ui",
-          "styleLibraryName": "theme-chalk"
-        }
+          "import", 
+          {
+              "libraryName": "vant",
+              "libraryDirectory": "es",
+              "style": true
+          }
       ]
+  ]
+  <%_ } _%>
+  <%_ if(UIfrag==='antVue'){ _%>
+  plugins: [
+      [
+          "import",
+            { 
+              "libraryName": "ant-design-vue", 
+              "libraryDirectory": "es", 
+              "style": "css" 
+          }
+      ] // `style: true` 会加载 less 文件
+  ]
+  <%_ } _%>
+  <%_ if(UIfrag==='eleUI'){ _%>
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
     ]
-    <%_ } _%>
+  ]
+  <%_ } _%>
   }
